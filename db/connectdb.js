f6de769593d9a0da52d335ef0922d5432b016b11
NileshSingh123 <ns800622@gmail.com>
2023-04-12 17:44:44 -0700

    live url

--- db/connectdb.js
+++ db/connectdb.js
@@ -2,12 +2,13 @@ const  mongoose  = require('mongoose')
 const monggose = require('mongoose')
 
 const url = 'mongodb://127.0.0.1:27017/OnlineCourse'
+const live_url ="mongodb+srv://ns800622:ram123@cluster0.h12n832.mongodb.net/OnlineCourse?retryWrites=true&w=majority"
 
 
 
 
 const connectDB =()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_url)
 
 
 
